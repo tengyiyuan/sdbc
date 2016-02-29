fa0357cb88cea5f20c1c1e31e37c55acdea61d57
tengyiyuan <363353765@qq.com>
2016-02-29 10:24:40 +0800

    frist demo

---
+++ test/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.test;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import com.ab.activity.AbActivity;
+import com.ab.view.titlebar.AbTitleBar;
+public class MainActivity extends AbActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setAbContentView(R.layout.activity_main);
+        AbTitleBar mAbTitleBar = this.getTitleBar();
+        mAbTitleBar.setTitleText("测试标题");
+        mAbTitleBar.setTitleBarGravity(Gravity.CENTER,Gravity.CENTER);
+        mAbTitleBar.setTitleBarBackgroundColor(Color.BLACK);
+    }
+}
